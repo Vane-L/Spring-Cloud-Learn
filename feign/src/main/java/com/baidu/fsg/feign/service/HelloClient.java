b06e2002a207a94466177b68eb400f54eb40baa6
wenhongliang <wenhong.liang@shopee.com>
2020-10-23 14:06:42 +0800

    update feign

--- feign/src/main/java/com/baidu/fsg/feign/service/HelloClient.java
+++ feign/src/main/java/com/baidu/fsg/feign/service/HelloClient.java
@@ -1,10 +1,10 @@
 package com.baidu.fsg.feign.service;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("hello-service")
+@FeignClient(name = "hello-service")
 public interface HelloClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/hello")
--- hello/src/main/java/com/baidu/fsg/controller/HelloController.java
+++ hello/src/main/java/com/baidu/fsg/controller/HelloController.java
@@ -2,18 +2,14 @@ package com.baidu.fsg.controller;
 
 import java.util.logging.Logger;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RestController
 public class HelloController {
@@ -24,10 +20,9 @@ public class HelloController {
     DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        HttpServletResponse httpServletResponse = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
-        int status = httpServletResponse.getStatus();
-        System.out.println(status);
+    public String hello(HttpServletRequest request, HttpServletResponse response) {
+        int status = response.getStatus();
+        System.out.println("status: " + status);
         return "Hello World!";
     }
 }
